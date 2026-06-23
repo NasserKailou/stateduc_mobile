2bb1b399ec584aae5caaaa366f84ce7d9fd5b5d7
NasserKailou <kailounasser@gmail.com>
2026-06-23 19:57:00 +0100

    20260623_MEN_BURUNDI

--- StatEduc_MEN_2025/connexion.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-#96;Sql Serveur;mssql;DESKTOP-AA23SDR\SQLEXPRESS;test;1234;BD_NIGER_EDUC
-
-?>
---
+++ StatEduc_burundi/client-side/js/menus/5/fr/4/2/_menu_saisie.js
@@ -0,0 +1,38 @@
+//Menu object creation
+            oCMenu=new makeCM("oCMenu") //Making the menu object. Argument: menuname
+            oCMenu.frames = 0
+            //Menu properties   
+            oCMenu.pxBetween =0
+            oCMenu.fromTop=6
+            oCMenu.rows=1 
+            oCMenu.offlineRoot="" 
+            oCMenu.onlineRoot="" 
+            oCMenu.resizeCheck=1
+            oCMenu.wait=300 
+            oCMenu.fillImg="image/cm_fill.gif"
+            oCMenu.zIndex=400
+            oCMenu.menuPlacement=0  // permet de contrôler (ou bloquer) la position à gauche pour éviter
+                                    // dans notre cas, que le menu ne se superpose pas au logo
+            
+            //Background bar properties
+            oCMenu.useBar=0         // 1 affiche la barre de menu, 0 la cache
+            oCMenu.barWidth="100%"
+            oCMenu.barHeight=50     // modifie la hauteur de la barre de menu
+            oCMenu.barClass="clBar" // classe css pour la barre
+            oCMenu.barX=0 
+            oCMenu.barY=50
+            oCMenu.barBorderX=0
+            oCMenu.barBorderY=0
+            oCMenu.barBorderClass=""
+                                                    
+            // Paramètres de cm_makeLevel(width, height, regClass, overClass, borderX, borderY, borderClass, rows, align, offsetX, offsetY, arrow, arrowWidth, arrowHeight, roundBorder)
+            oCMenu.level[0]=new cm_makeLevel(90,21,"clT","clTover",1,1,"clB",0,"bottom",0,0,0,0,0);
+            oCMenu.level[2]=new cm_makeLevel(110,22,"clS2","clS2over");
+            oCMenu.level[3]=new cm_makeLevel(140,22);oCMenu.fromLeft=175;
+            oCMenu.level[1]=new cm_makeLevel(102,22,"clS","clSover",1,1,"clB",0,"right",0,0,"client-side/image/menu_arrow.php",10,10);
+oCMenu.makeMenu('18104','1030','Identification_ALPHA','questionnaire.php?theme=18104&type_ent_stat=2','','220');
+oCMenu.makeMenu('18204','1030','Environnement_Alpha','questionnaire.php?theme=18204&type_ent_stat=2','','220');
+oCMenu.makeMenu('18304','1030','Infos Etablissement','questionnaire.php?theme=18304&type_ent_stat=2','','220');
+
+//Leave this line - it constructs the menu
+            oCMenu.construct();
